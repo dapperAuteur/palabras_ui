33012786cf9344f300e3dca9f63fe100aea3a3f8
Anthony McDonald <a@awews.com>
2018-02-14 06:14:34 -0500

    add propTypes to Letters.js, attempt to put all letters on same line

--- src/components/Games/Letters.js
+++ src/components/Games/Letters.js
@@ -5,24 +5,38 @@ import './Letter.css';
 class Letters extends Component {
   constructor(props) {
     super(props);
+    const { _id, attempts, bulls, cows, guesses, userId, score, winning_word, won, word_to_consider_for_library } = this.props.data.props.game;
     this.state = {
+      _id,
+      p: 'games/',
       letter0: '',
       letter1: '',
       letter2: '',
       letter3: '',
       letters: [
         "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-      ]
+      ],
+      attempts,
+      bulls,
+      cows,
+      guesses,
+      userId,
+      score,
+      winning_word,
+      won,
+      word_to_consider_for_library
     }
     this.handleChange = this.handleChange.bind(this);
-    this.handleLetter = this.handleLetter.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleChange(e){
     console.log(e.target.value);
     this.setState({ [e.target.name]: e.target.value });
   }
-  handleLetter(){
-
+  handleSubmit(e){
+    e.preventDefault();
+    let { p, ...pObj } = { p, ...this.state };
+    console.log(p, pObj);
   }
 
   render() {
@@ -32,7 +46,7 @@ class Letters extends Component {
     return (
       <div className='word-form-container'>
         <form className='form-letters' onSubmit={ this.handleSubmit }>
-          <div className='form-letter'>
+          <span className='form-letter'>
             <label htmlFor='letter0'>0</label>
             <select
               id='letter0'
@@ -45,8 +59,8 @@ class Letters extends Component {
                 <option key={ i } value={ letter }>{ letter }</option>
               )))}
             </select>
-          </div>
-          <div className='form-letter'>
+          </span>
+          <span className='form-letter'>
             <label htmlFor='letter1'>1</label>
             <select
               id='letter1'
@@ -59,8 +73,8 @@ class Letters extends Component {
                 <option key={ i } value={ letter }>{ letter }</option>
               )))}
             </select>
-          </div>
-          <div className='form-letter'>
+          </span>
+          <span className='form-letter'>
             <label htmlFor='letter2'>2</label>
             <select
               id='letter2'
@@ -73,8 +87,8 @@ class Letters extends Component {
                 <option key={ i } value={ letter }>{ letter }</option>
               )))}
             </select>
-          </div>
-          <div className='form-letter'>
+          </span>
+          <span className='form-letter'>
             <label htmlFor='letter3'>3</label>
             <select
               id='letter3'
@@ -87,7 +101,7 @@ class Letters extends Component {
                 <option key={ i } value={ letter }>{ letter }</option>
               )))}
             </select>
-          </div>
+          </span>
           <button
             type="submit"
             className="buttons"
@@ -102,21 +116,49 @@ class Letters extends Component {
 }
 
 Letters.propTypes = {
+  _id: PropTypes.string.isRequired,
+  p: PropTypes.string.isRequired,
   letter0: PropTypes.string.isRequired,
   letter1: PropTypes.string.isRequired,
   letter2: PropTypes.string.isRequired,
   letter3: PropTypes.string.isRequired,
-  letters: PropTypes.array.isRequired
+  letters: PropTypes.arrayOf(PropTypes.string).isRequired,
+  attempts: PropTypes.number,
+  bulls: PropTypes.number,
+  cows: PropTypes.number,
+  guesses: PropTypes.arrayOf(PropTypes.string).isRequired,
+  userId: PropTypes.string.isRequired,
+  score: PropTypes.number,
+  winning_word: PropTypes.shape({
+    0: PropTypes.string.isRequired,
+    1: PropTypes.string.isRequired,
+    2: PropTypes.string.isRequired,
+    3: PropTypes.string.isRequired
+  }),
+  won: PropTypes.bool,
+  word_to_consider_for_library: PropTypes.arrayOf(PropTypes.string).isRequired
 }
 
 Letters.defaultProps = {
+  onSave() {},
+  _id: '',
+  p: 'games/',
   letter0: 'F',
   letter1: 'A',
   letter2: 'R',
   letter3: 'T',
   letters: [
     "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-  ]
+  ],
+  attempts: 0,
+  bulls: 0,
+  cows: 0,
+  guesses: [],
+  userId: '',
+  score: 0,
+  winning_word: {},
+  won: false,
+  word_to_consider_for_library: []
 }
 
 export default Letters;
